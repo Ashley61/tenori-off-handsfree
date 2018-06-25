fc109aa6bd8f6480750f492a1a9af84df54ded3a
Glitch (tenori-on) <none>
2018-06-25 21:10:49 +0000

    🌿🕍 Checkpoint ./style.css:293015/128 ./script.js:293015/2161 ./index.html:293015/8311

--- script.js
+++ script.js
@@ -3,13 +3,29 @@ init();
 draw();
 
 function init() {
+  const container = document.getElementById('container');
+  
+  // Draw the grid.
   for (let i = 0; i < 16; i++) {
     let row = [];
     dots.push(row);
+    const rowEl = document.createElement('div');
+    rowEl.classList.add('row');
+    container.appendChild(rowEl);
+    
     for (let j = 0; j < 16; j++) {
       dots[i][j] = 0;
+      const button = document.createElement('button');
+      button.classList.add('pixel');
+      button.dataset.row = i;
+      button.dataset.col = j;
+      rowEl.appendChild(button);
     }
   }
+  
+  document.getElementById('container').addEventListener('click', (event) => {
+    
+  });
 }
 
 function enableSome() {
@@ -28,4 +44,19 @@ function draw() {
       }
     }
   }
+}
+
+function activate(event) {
+  const button = event.target;
+    
+  // We only care about clicking on the buttons, not the container itself.
+  if (button.localName !== 'button') {
+    return;
+  }
+  
+  const isOn = button.classList.contains('on');
+  if (isOn) {
+    
+  dots[button.dataset.row][button.dataset.col] = !button.classList.contains('on');
+  draw();
 }
