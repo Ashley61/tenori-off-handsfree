2c3d82cacc01306c979204ee56d8b3884d83126e
Glitch (tenori-on) <none>
2018-06-25 21:00:04 +0000

    📷💡 Checkpoint ./style.css:293015/267 ./script.js:293015/1233 ./index.html:293015/15831

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+let dots = [];
+init();
+draw();
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+function init() {
+  for (let i = 0; i < 16; i++) {
+    let row = [];
+    dots.push(row);
+    for (let j = 0; j < 16; j++) {
+      dots[i][j] = 0;
+    }
+  }
+}
+
+function enableSome() {
+  dots[3][5] = 1;
+  draw();
+}
+
+function draw() {
+  const rows = document.querySelectorAll('.container > .row');
+  for (let i = 0; i < 16; i++) {
+    const pixels = rows[i].querySelectorAll('.pixel');
+    for (let j = 0; j < 16; j++) {
+      pixels[j].classList.remove('on');
+      if (dots[i][j]) {
+        pixels[j].classList.add('on');
+      }
+    }
+  }
+}
