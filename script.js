b05cafd300fc30e9880d15b8d32c27734df226e4
Glitch (toneori-js) <none>
2018-06-26 04:32:21 +0000

    🎽🎇 Checkpoint ./script.js:293015/6244 ./index.html:293015/98

--- script.js
+++ script.js
@@ -8,16 +8,62 @@ const SYNTH = ['B4', 'A4', 'G4', 'F4', 'E4', 'D4', 'C4',
                'B3', 'A3', 'G3', 'F3', 'E3', 'D3', 'C3', 
                'B2', 'A2', 'G2', 'F2'];
 
-const DRUM_CLASSES = [
-  'Kick',
-  'Snare',
-  'Hi-hat closed',
-  'Hi-hat open',
-  'Tom low',
-  'Tom mid',
-  'Tom high',
-  'Clap',
-  'Rim'
+// From https://codepen.io/teropa/pen/JLjXGK
+let sampleBaseUrl = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/969699';
+
+let reverb = new Tone.Convolver(
+  `${sampleBaseUrl}/small-drum-room.wav`
+).toMaster();
+reverb.wet.value = 0.35;
+
+let snarePanner = new Tone.Panner().connect(reverb);
+new Tone.LFO(0.13, -0.25, 0.25).connect(snarePanner.pan).start();
+
+let DRUMS = [
+  new  Tone.Player({
+    med: `${sampleBaseUrl}/808-kick-vm.mp3`,
+    low: `${sampleBaseUrl}/808-kick-vl.mp3`
+  }).toMaster(),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/flares-snare-vh.mp3`,
+    med: `${sampleBaseUrl}/flares-snare-vm.mp3`,
+    low: `${sampleBaseUrl}/flares-snare-vl.mp3`
+  }).connect(snarePanner),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/808-hihat-vh.mp3`,
+    med: `${sampleBaseUrl}/808-hihat-vm.mp3`,
+    low: `${sampleBaseUrl}/808-hihat-vl.mp3`
+  }).connect(new Tone.Panner(-0.5).connect(reverb)),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/808-hihat-open-vh.mp3`,
+    med: `${sampleBaseUrl}/808-hihat-open-vm.mp3`,
+    low: `${sampleBaseUrl}/808-hihat-open-vl.mp3`
+  }).connect(new Tone.Panner(-0.5).connect(reverb)),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/slamdam-tom-low-vh.mp3`,
+    med: `${sampleBaseUrl}/slamdam-tom-low-vm.mp3`,
+    low: `${sampleBaseUrl}/slamdam-tom-low-vl.mp3`
+  }).connect(new Tone.Panner(-0.4).connect(reverb)),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/slamdam-tom-mid-vh.mp3`,
+    med: `${sampleBaseUrl}/slamdam-tom-mid-vm.mp3`,
+    low: `${sampleBaseUrl}/slamdam-tom-mid-vl.mp3`
+  }).connect(reverb),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/slamdam-tom-high-vh.mp3`,
+    med: `${sampleBaseUrl}/slamdam-tom-high-vm.mp3`,
+    low: `${sampleBaseUrl}/slamdam-tom-high-vl.mp3`
+  }).connect(new Tone.Panner(0.4).connect(reverb)),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/909-clap-vh.mp3`,
+    med: `${sampleBaseUrl}/909-clap-vm.mp3`,
+    low: `${sampleBaseUrl}/909-clap-vl.mp3`
+  }).connect(new Tone.Panner(0.5).connect(reverb)),
+  new  Tone.Player({
+    high: `${sampleBaseUrl}/909-rim-vh.wav`,
+    med: `${sampleBaseUrl}/909-rim-vm.wav`,
+    low: `${sampleBaseUrl}/909-rim-vl.wav`
+  }).connect(new Tone.Panner(0.5).connect(reverb))
 ];
 
 init();
@@ -154,7 +200,8 @@ function play() {
     
     // Play the note
     if (playNoteOnThisColumn !== -1) {
-      synth.triggerAttackRelease(SYNTH[playNoteOnThisColumn], '16n');
+      //synth.triggerAttackRelease(SYNTH[playNoteOnThisColumn], '16n');
+      DRUMS[0].get('med').start();
     }
     
     draw();
