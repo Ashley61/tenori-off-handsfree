bec7becce3858c335113a27cec1f1c3611ef8a2f
Glitch (tenori-off) <none>
2018-06-27 23:09:58 +0000

    🍵🌌 Checkpoint ./helpers.js:293015/2111

--- helpers.js
+++ helpers.js
@@ -15,27 +15,19 @@ class NoiseyMakey {
     this.magentaPlayer = new mm.Player();
     
     // From https://github.com/tensorflow/magenta-js/blob/master/music/src/core/data.ts#L35
-    this.magentaPitches = [35, 38, 42, 46, 45, 48, 50, 49, 51]; //35, 44, 67, 29, 47, 30, 55];
+    this.magentaPitches = [35, 38, 42, 46, 45, 48, 50, 49, 51]; //35, 27, 29, 47, 49, 51, 42;
 
+    
     // From https://codepen.io/teropa/pen/JLjXGK. Thanks teropa!! <3
     let sampleBaseUrl = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/969699';
     this.drumSounds = [
       new Tone.Player(`${sampleBaseUrl}/808-kick-vm.mp3`).toMaster(),
       new Tone.Player(`${sampleBaseUrl}/flares-snare-vh.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/808-hihat-vh.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/808-hihat-open-vh.mp3`).toMaster(),
       new Tone.Player(`${sampleBaseUrl}/slamdam-tom-low-vh.mp3`).toMaster(),
       new Tone.Player(`${sampleBaseUrl}/slamdam-tom-mid-vm.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/slamdam-tom-high-vh.mp3`).toMaster(),
       new Tone.Player(`${sampleBaseUrl}/909-clap-vh.mp3`).toMaster(),
       new Tone.Player(`${sampleBaseUrl}/909-rim-vh.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/808-kick-vl.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/flares-snare-vl.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/808-hihat-vl.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/808-hihat-open-vl.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/slamdam-tom-low-vl.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/slamdam-tom-mid-vl.mp3`).toMaster(),
-      new Tone.Player(`${sampleBaseUrl}/slamdam-tom-high-vl.mp3`).toMaster(),
+      new Tone.Player(`${sampleBaseUrl}/808-kick-vl.mp3`).toMaster()
     ];
   }
   
@@ -61,13 +53,14 @@ class NoiseyMakey {
   playDrum(which) {
     if (which < this.magentaPitches.length) {
       this.magentaPlayer.drumKit.playNote(this.magentaPitches[which]);
-    } el
-    //this.drumSounds[which].start(Tone.now(), 0);
+    } else {
+      this.drumSounds[which - this.magentaPitches.length].start(Tone.now(), 0);
+    }
   }
   
   // Drums need to be cleared since they're just a looping mp3.
   resetDrums() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < this.drumSounds.length; i++) {
       this.drumSounds[i].stop();
     }
   }
@@ -155,12 +148,7 @@ class Board {
   getSynthSequence() {
     const sequence = {notes:[], quantizationInfo: {stepsPerQuarter: 4}};
     
-    // From https://github.com/tensorflow/magenta-js/blob/master/music/src/core/data.ts#L35
-    //
-    const drumPitches = [36, 48, 42, 50, 45, 49, 51, 38, 46, 35, 27, 28, 31, 32, 33, 34, 37, 39];
-    //const drumPitches = [36, 48, 42, 50, 45, 49, 51, 38, 46, 35, 27, 28, 31, 32, 33, 34, 37, 39];
-    
-    
+    const drumPitches = [36, 38, 42, 46, 45, 48, 50, 49, 51, 35, 27, 29, 47, 49, 51, 42];
     for (let i = 0; i < 16; i++) {
       for (let j = 0; j < 16; j++) {
         // Found a synth note!
@@ -184,8 +172,7 @@ class Board {
       console.log('Something mysterious went wrong, bailing');
     }
     
-    // From https://github.com/tensorflow/magenta-js/blob/master/music/src/core/data.ts#L35
-    const drumPitches = [36, 48, 42, 50, 45, 49, 51, 38, 46, 35, 27, 28, 31, 32, 33, 34, 37, 39];
+    const drumPitches = [36, 38, 42, 46, 45, 48, 50, 49, 51, 35, 27, 29, 47, 49, 51, 42];
     for (let i = 0; i < sequence.notes.length; i++) {
       // note = {pitch: 36, quantizedStartStep: 1, quantizedEndStep: 2, isDrum: true}
       const note = sequence.notes[i];      
