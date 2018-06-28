aa954b668c7a38424f316d31d1c4f9c9fd9d028c
Glitch (tenori-off) <none>
2018-06-28 01:37:40 +0000

    ☄️🍂 Checkpoint ./helpers.js:293015/136

--- helpers.js
+++ helpers.js
@@ -23,9 +23,9 @@ class NoiseyMakey {
     // From https://github.com/tensorflow/magenta-js/blob/master/music/src/core/data.ts#L35
     this.magentaPitches = [36, 38, 42, 46, 45, 48, 50, 49, 51, /*repeat*/ 35, 27, 29, 47, 30, 52, 44]; 
 
-    const envelope = {attack: 0, decay: 4, sustain: 0, release: 0.1, attackCurve: 'exponential'};
+    const envelope = {attack: 0, decay: 2, sustain: 0, release: 0, attackCurve: 'exponential'};
      
-    const clap = new Tone.NoiseSynth({envelope}).toMaster();
+    const clap = new Tone.NoiseSynth({noise: 'brown', envelope}).toMaster();
     debugger
   }
   
