d59798863b2b0b3f51a1dbbba2cb60b5a4acadc5
Glitch (tenori-off) <none>
2018-10-28 22:23:04 +0000

    🎺👋 Checkpoint ./script.js:1601221/346 ./handsfree.js:1601221/252

--- handsfree.js
+++ handsfree.js
@@ -1,1 +1,11 @@
 window.handsfree = new Handsfree({debug: true})
+
+// Click on containers
+handsfree.use({
+  name: 'clickContainers',
+  onFrame: faces => {
+    faces.forEach(face => {
+      if (face.cursor.
+    })
+  }
+})
