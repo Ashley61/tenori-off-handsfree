83848c262f7d2441d6f7facedd923d3d95e62665
Glitch (tenori-off) <none>
2018-10-28 22:34:02 +0000

    😬🎲 Checkpoint ./handsfree.js:1601221/826 ./handsfree.css:1601221/150 ./index.html:1601221/639

--- handsfree.js
+++ handsfree.js
@@ -2,10 +2,23 @@ window.handsfree = new Handsfree({debug: true})
 
 // Click on containers
 handsfree.use({
+  // The unique name of the plugin
   name: 'clickContainers',
+
+  // This is called on every frame, with an array of face objects
   onFrame: faces => {
+    // Lets loop through each detected face
     faces.forEach(face => {
-      if (face.cursor.
+      // Let's detect the mousedown state
+      if (face.cursor.state.mouseDown) {
+        // Dispatch a mousedown at the point of click, bubbling in case we click a span element inside a button for example
+        face.cursor.$target.dispatchEvent(new MouseEvent('mousedown', {
+          bubbles: true,
+          cancelable: true,
+          clientX: face.cursor.x,
+          clientY: face.cursor.y
+        }))
+      }
     })
   }
 })
